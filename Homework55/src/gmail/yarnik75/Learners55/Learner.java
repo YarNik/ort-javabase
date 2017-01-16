a601f9521d27a9d125fc0d9c342352d2b41cd635
YarNik <yarnik75@gmail.com>
2017-01-16 17:40:50 +0200

    Solved task#12.

---
+++ Homework55/src/gmail/yarnik75/Learners55/Learner.java
@@ -0,0 +1,17 @@
+package gmail.yarnik75.Learners55;
+
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Learner {
+	private TreeMap <String, Integer[]> map = new TreeMap<String, Integer[]>();
+	
+	public void add(String name, int r1, int r2, int r3, int r4, int r5){
+		Integer [] rating = {r1, r2, r3, r4, r5};
+		map.put(name, rating);
+	}
+	
+	public Set<String> view(){
+		return map.keySet();
+	}
+}
